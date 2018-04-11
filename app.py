1304aee48b45def1f7b2bb01b5a4e2a0c677649a
Alex Antão <alex.antao@serpro.gov.br>
2018-04-11 16:26:55 -0300

    Aula 3 - Exercicio 2

--- app.py
+++ app.py
@@ -1,14 +1,24 @@
 import logging
+import datetime
+
 from flask import Flask, request
 from logging.handlers import RotatingFileHandler
+from flask.ext.redis import FlaskRedis
+
 
 app = Flask(__name__)
+app.config['REDIS_URL'] = "redis://redis:6379/0"
+
+redis_store = FlaskRedis(app)
 
 handler = RotatingFileHandler('/tmp/foo.log', maxBytes=10000, backupCount=1)
 handler.setLevel(logging.INFO)
 app.logger.addHandler(handler)
 
+redis_store.set('Start Time', datetime.datetime.now())
+
+
 @app.route("/")
 def hello():
     app.logger.error(('O Referrer foi: {}'.format(request.referrer)))
-    return " ------ > Olá. Mundo (versão 2)! <----- "
+    return " ------ > Aplicação com Redis ! <----- "
